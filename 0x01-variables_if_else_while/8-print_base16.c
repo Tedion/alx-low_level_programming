d53b0844ce1b6818fff87eef01aada9716f1f9e5
Tedion <tedionabera@gmail.com>
2022-03-16 16:41:47 -0700

    My commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description:prints numbers of base16 in lowercase
+ *
+ *Return: Always zero
+ */
+int main(void)
+{
+	int dig, letterhex, n;
+
+	for (dig = 48, n = 0; n < 10; dig++, n++)
+	{
+		putchar(dig);
+	}
+
